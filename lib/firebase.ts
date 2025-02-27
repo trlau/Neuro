43ea315e8e2bbb702b8e897ca32f355189703da4
PNY92 <pxitrbx@gmail.com>
2025-02-27 09:39:16 +0800

    Added Firebase Config and Docker Compose

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDx2JFQAYFfzSpGDlL4KjLhL_Ru5zY8KuY",
+  authDomain: "aira-api.firebaseapp.com",
+  databaseURL: "https://aira-api-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "aira-api",
+  storageBucket: "aira-api.firebasestorage.app",
+  messagingSenderId: "1026132151304",
+  appId: "1:1026132151304:web:0b8b75786fd018e8e7d5bd"
 };
 
 const app = initializeApp(firebaseConfig);
